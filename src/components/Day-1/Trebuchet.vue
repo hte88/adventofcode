ff6d3bf4cc1abf035bfc71e3c8d7b2790f97f30a
hte88 <olivier.marion88@gmail.com>
2023-12-02 11:44:50 +0100

    feat: trebuchet init

---
+++ src/components/Day-1/Trebuchet.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const input = ref('')
+let nums = "0123456789";
+
+function count(){
+    const array = input.value.split('\n')
+    console.log(array);
+}
+</script>
+<template>
+  <div>
+    <textarea v-model="input" />
+    <button @click="count">Count</button>
+  </div>
+</template>
