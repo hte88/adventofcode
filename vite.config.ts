b636e84ceb0d77dec895fd788b8586f6612db482
hte88 <olivier.marion88@gmail.com>
2023-12-05 17:12:45 +0100

    feat: auto import + tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import path from 'node:path';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: ['vue', 'vue-router', '@vueuse/head', '@vueuse/core'],
+      dts: './src/auto-imports.d.ts',
+      dirs: ['./src/composables', './src/stores', './src/plugins'],
+      vueTemplate: true,
+      eslintrc: {
+        enabled: true,
+        filepath: './.eslintrc-auto-import.json',
+        globalsPropValue: true,
+      },
+    }),
+    Components({
+      dts: true,
+    }),
+  ],
+  resolve: {
+    alias: {
+      '~/': `${path.resolve(__dirname, 'src')}/`,
+    },
+  },
+});
