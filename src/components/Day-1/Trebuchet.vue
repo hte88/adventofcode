9c2ce3f3f43aacea452d527b972d4e6af8ef1584
hte88 <olivier.marion88@gmail.com>
2023-12-05 19:57:18 +0100

    feat: trebuchet step 2

--- src/components/Day-1/Trebuchet.vue
+++ src/components/Day-1/Trebuchet.vue
@@ -2,13 +2,18 @@
 const { fileContent, handleFileChange } = useFileReader();
 
 const total = ref<number>(0);
+const letters = { zero: 0, one: 1, two: 2, three: 3, four: 4, five: 5, six: 6, seven: 7, eight: 8, nine: 9 };
 
 function isNumber(x: string) {
   const nums = '0123456789';
   return nums.includes(x) ? true : false;
 }
 
-function mergeNumbers(nums: string[]) {
+function isNumberToLetter(str: string): number {
+  return letters[str] ?? 0;
+}
+
+function mergeNumbers(nums: string[]): number {
   if (nums.length >= 3) {
     return parseInt(nums[0] + nums.slice(-1));
   } else if (nums.length === 1) {
@@ -18,12 +23,58 @@ function mergeNumbers(nums: string[]) {
   }
 }
 
-function trebuchet() {
+function extractWords(str: string): number[] {
+  const result: number[] = [];
+  const strLower = str.toLowerCase();
+
+  for (let i = 0; i < str.length; i++) {
+    for (let j = i + 1; j <= str.length; j++) {
+      const subStr = strLower.substring(i, j);
+      const num = isNumberToLetter(subStr);
+      if (num) {
+        result.push(num);
+      }
+    }
+  }
+
+  return result;
+}
+
+function stepOne() {
   const words = fileContent.value?.split('\n');
 
   const numbers = words
     ?.map((str) => mergeNumbers([...str].filter((x) => (isNumber(x) ? x : ''))))
     ?.filter((num) => Number.isInteger(num));
+
+  total.value = numbers?.reduce((x, y) => x + y, 0) ?? 0;
+}
+
+function stepTwo() {
+  const words = fileContent.value?.split('\n');
+
+  const resultat = words?.map((word) => {
+    const splitByDigital = word.match(/[a-zA-Z]+|\d+/g);
+    return (
+      splitByDigital
+        ?.map((str: string) => {
+          if (Number.isInteger(parseInt(str))) {
+            return [...(str + '')].map((str) => parseInt(str));
+          } else if (Object.keys(letters).find((motInList) => str.includes(motInList))) {
+            return extractWords(str);
+          } else {
+            return null;
+          }
+        })
+        ?.filter((str) => str)
+        .flat() ?? []
+    );
+  });
+
+  const numbers = resultat
+    ?.map((str) => mergeNumbers([...str].filter((x) => (isNumber(x) ? x : ''))))
+    ?.filter((num) => Number.isInteger(num));
+  console.log(numbers);
 
   total.value = numbers?.reduce((x, y) => x + y, 0) ?? 0;
 }
@@ -39,9 +90,16 @@ function trebuchet() {
     <button
       v-if="fileContent"
       class="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white bg-blue-600"
-      @click.prevent="trebuchet()"
+      @click.prevent="stepOne()"
     >
       Go step 1
+    </button>
+    <button
+      v-if="fileContent"
+      class="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white bg-blue-600"
+      @click.prevent="stepTwo()"
+    >
+      Go step 2
     </button>
 
     <p><b>Result:</b> {{ total }}</p>
