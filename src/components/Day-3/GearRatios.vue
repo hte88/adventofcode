17dcd728f2cb74b66f3d03f71c01e564b10c3d1c
hte88 <olivier.marion88@gmail.com>
2023-12-12 19:15:31 +0100

    feat: step 2

--- src/components/Day-3/GearRatios.vue
+++ src/components/Day-3/GearRatios.vue
@@ -2,6 +2,10 @@
 import { isNumber, extractSymboles, createMapFile } from '~/composables/useLib';
 
 interface IMap extends Map<string, string> {}
+type TMapItem = {
+  number: string;
+  position: string;
+};
 
 const { fileContent, handleFileChange } = useFileReader();
 
@@ -24,21 +28,22 @@ function getSubMatrix(map: IMap, startRow: number, endRow: number, startCol: num
   return subMatrix;
 }
 
-function parseSubMatrix(subMatrix: IMap, symboles: string[]): boolean {
-  let findSymbole = false;
-
-  subMatrix.forEach((element: string) => {
-    if (symboles.includes(element)) {
-      findSymbole = true;
-    }
-  });
-  return findSymbole;
-}
-
-function getNumberFromMap(mapFile: IMap, nums: string, rowIndex: number, colIndex: number, symboles: string[]): string {
+function getNumberFromMap(mapFile: IMap, nums: string, rowIndex: number, colIndex: number, symboles: string[]) {
   const subMatrix = getSubMatrix(mapFile, rowIndex - 1, rowIndex + 1, colIndex - (nums.length + 1), colIndex);
 
-  return parseSubMatrix(subMatrix, symboles) ? nums : '';
+  const position = parseSubMatrix(subMatrix, symboles);
+
+  return position ? ({ number: nums, position } as TMapItem) : '';
+}
+
+function parseSubMatrix(subMatrix: IMap, symboles: string[]): string {
+  let position = '';
+  subMatrix.forEach((element: string, keyPosition: string) => {
+    if (symboles.includes(element)) {
+      position = keyPosition;
+    }
+  });
+  return position;
 }
 
 function stepOne() {
@@ -82,6 +87,70 @@ function stepOne() {
     .map((str) => parseInt(str))
     .reduce((a, b) => a + b, 0);
 }
+
+function stepTwo() {
+  const symboles = ['*'];
+  const mapFile = createMapFile(lines.value);
+
+  const numbers = lines.value.map((str, rowIndex) => {
+    const char = str.split('');
+    let nums = '';
+    const numbers = [];
+
+    for (let colIndex = 0; colIndex < char.length; colIndex++) {
+      const element = char[colIndex];
+
+      if (isNumber(element)) {
+        nums += element;
+        if (colIndex + 1 === lineLength.value) {
+          const getNbr = getNumberFromMap(mapFile, nums, rowIndex, colIndex + 1, symboles);
+          if (getNbr && getNbr.number) {
+            numbers.push({ number: getNbr.number, position: getNbr.position } as TMapItem);
+          }
+          nums = '';
+        }
+      } else {
+        if (nums.length) {
+          const getNbr = getNumberFromMap(mapFile, nums, rowIndex, colIndex, symboles);
+          if (getNbr && getNbr.number) {
+            numbers.push({ number: getNbr.number, position: getNbr.position } as TMapItem);
+          }
+        }
+        nums = '';
+      }
+    }
+
+    return numbers;
+  });
+
+  const savePositionDuplicates = numbers.flat().filter((numsArray, index, arr) => {
+    if (numsArray) {
+      const currentPos = numsArray?.position;
+      return arr.some(
+        (otherArray, otherIndex) => otherIndex !== index && otherArray && otherArray?.position === currentPos,
+      );
+    }
+    return false;
+  });
+
+  const multiplyByPos: { [position: string]: number } = {};
+
+  savePositionDuplicates.forEach((numsArray) => {
+    const position = numsArray?.position;
+    const number = parseInt(numsArray?.number);
+
+    if (!isNaN(number)) {
+      if (multiplyByPos[position] === undefined) {
+        multiplyByPos[position] = number;
+      } else {
+        multiplyByPos[position] *= number;
+      }
+    }
+  });
+
+  const result = Object.values(multiplyByPos).reduce((acc, product) => acc + product, 0);
+  total.value = result;
+}
 </script>
 <template>
   <Card title="Day 3: Gear Ratios" :result="total">
@@ -94,6 +163,7 @@ function stepOne() {
     </template>
     <template #actions>
       <button type="button" class="btn join-item btn-primary" @click.prevent="stepOne()">Step 1</button>
+      <button type="button" class="btn join-item btn-primary" @click.prevent="stepTwo()">Step 2</button>
     </template>
   </Card>
 </template>
