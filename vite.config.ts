58dcca3c34bf0d9240528fd047dbf573561a6648
hte88 <olivier.marion88@gmail.com>
2023-12-13 11:22:40 +0100

    feat: prettier tailwindcss + defineModels

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,11 @@ import Components from 'unplugin-vue-components/vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      script: {
+        defineModel: true,
+      },
+    }),
     AutoImport({
       imports: ['vue', 'vue-router', '@vueuse/head', '@vueuse/core'],
       dts: './src/auto-imports.d.ts',
