f052839daf870ae0cf17cd764e707d3ef96c5efd
hte88 <olivier.marion88@gmail.com>
2023-12-14 14:06:23 +0100

    fix: ts

--- src/components/Day-1/Trebuchet.vue
+++ src/components/Day-1/Trebuchet.vue
@@ -2,7 +2,9 @@
 const { fileContent, handleFileChange } = useFileReader();
 
 const total = ref<number>(0);
-const letters = { zero: 0, one: 1, two: 2, three: 3, four: 4, five: 5, six: 6, seven: 7, eight: 8, nine: 9 };
+const letters = { zero: 0, one: 1, two: 2, three: 3, four: 4, five: 5, six: 6, seven: 7, eight: 8, nine: 9 } as {
+  [key: string]: number;
+};
 const example_1 = ref(`1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
@@ -57,7 +59,7 @@ function stepOne() {
   const words = file?.split('\n');
 
   const numbers = words
-    ?.map((str) => mergeNumbers([...str].filter((x) => (isNumber(x) ? x : ''))))
+    ?.map((str) => mergeNumbers([...str].filter((x) => (isNumber(x) ? parseInt(x) : ''))))
     ?.filter((num) => Number.isInteger(num));
 
   total.value = numbers?.reduce((x, y) => x + y, 0) ?? 0;
@@ -74,7 +76,7 @@ function stepTwo() {
         ?.map((str: string) => {
           if (Number.isInteger(parseInt(str))) {
             return [...(str + '')];
-          } else if (Object.keys(letters).find((motInList) => str.includes(motInList))) {
+          } else if (Object.keys(letters).find((word) => str.includes(word))) {
             return extractWords(str);
           } else {
             return null;
@@ -84,9 +86,10 @@ function stepTwo() {
         .flat() ?? []
     );
   });
+  console.log(resultat);
 
   const numbers = resultat
-    ?.map((str) => mergeNumbers([...str].filter((x) => (isNumber(x) ? x : ''))))
+    ?.map((str) => mergeNumbers([...(str + '')].filter((x) => (isNumber(x) ? x : ''))))
     ?.filter((num) => Number.isInteger(num));
 
   total.value = numbers?.reduce((x, y) => x + y, 0) ?? 0;
