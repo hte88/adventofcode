028b216e060fd40acf03f41c3ea848d1cbf9d382
hte88 <olivier.marion88@gmail.com>
2023-12-14 13:13:34 +0100

    feat: Camel Cards step 1

--- src/components/Day-7/CamelCards.vue
+++ src/components/Day-7/CamelCards.vue
@@ -15,8 +15,68 @@ const lines = computed(() => {
   return selectedInput.value ? file?.split('\n') ?? [] : file?.split('\n').slice(0, -1) ?? [];
 });
 
+function ranking(hand) {
+  const groupByChar = {};
+
+  for (const card of hand) {
+    if (!groupByChar[card]) {
+      groupByChar[card] = 1;
+    } else {
+      groupByChar[card]++;
+    }
+  }
+
+  const keysValues = Object.keys(groupByChar);
+
+  switch (true) {
+    case keysValues.some((value) => groupByChar[value] === 5): //Five of a kind
+      return 6;
+    case keysValues.some((value) => groupByChar[value] === 4): //Four of a kind
+      return 5;
+    case keysValues.some((value) => groupByChar[value] === 3) && keysValues.some((value) => groupByChar[value] === 2): //Full house
+      return 4;
+    case keysValues.some((value) => groupByChar[value] === 3): //Three of a kind
+      return 3;
+    case keysValues.filter((value) => groupByChar[value] === 2).length === 2: //Two pair
+      return 2;
+    case keysValues.some((value) => groupByChar[value] === 2): //One pair
+      return 1;
+    default: //High card
+      return 0;
+  }
+}
+
+function order(a, b) {
+  const order = 'AKQJT98765432';
+
+  for (let i = 0; i < Math.min(a.length, b.length); i++) {
+    const diff = order.indexOf(b[i]) - order.indexOf(a[i]);
+    if (diff !== 0) {
+      return diff;
+    }
+  }
+
+  return 0;
+}
+
 function stepOne() {
-  // total.value = result.reduce((a: number, b: number) => a + b, 0);
+  const hands = lines.value.map((line) => {
+    const str = line.split(' ');
+    return { hand: str[0], bid: parseInt(str[1]) };
+  });
+
+  hands.sort((a, b) => {
+    const rangA = ranking(a.hand);
+    const rangB = ranking(b.hand);
+
+    if (rangA !== rangB) {
+      return rangA - rangB;
+    }
+
+    return order(a.hand, b.hand);
+  });
+
+  total.value = hands.reduce((a, b, i) => a + b.bid * (i + 1), 0);
 }
 
 function stepTwo() {
@@ -28,7 +88,7 @@ function stepTwo() {
     v-model:example="example"
     title="Day 7: Camel Cards"
     :result="total"
-    :rating="0"
+    :rating="1"
     @selected-input="selectedInput = $event"
   >
     <template #inputFile>
@@ -40,7 +100,7 @@ function stepTwo() {
     </template>
     <template #actions>
       <button type="button" class="btn btn-primary join-item" @click.prevent="stepOne()">Step 1</button>
-      <button type="button" class="btn btn-primary join-item" @click.prevent="stepTwo()">Step 2</button>
+      <!-- <button type="button" class="btn btn-primary join-item" @click.prevent="stepTwo()">Step 2</button> -->
     </template>
   </Card>
 </template>
